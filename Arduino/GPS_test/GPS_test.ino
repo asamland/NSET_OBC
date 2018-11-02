57fa8733300e4de4d119cf098317450d0f38c079
Alex Samland <alexandersamland2020@u.northwestern.edu>
2018-11-02 16:31:17 -0500

    arduino code

---
+++ Arduino/GPS_test/GPS_test.ino
@@ -0,0 +1,78 @@
+#include <SparkFun_I2C_GPS_Arduino_Library.h> //Use Library Manager or download here: https://github.com/sparkfun/SparkFun_I2C_GPS_Arduino_Library
+I2CGPS myI2CGPS; //Hook object to the library
+
+#include <TinyGPS++.h> //From: https://github.com/mikalhart/TinyGPSPlus
+TinyGPSPlus gps; //Declare gps object
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("GTOP Read Example");
+
+  if (myI2CGPS.begin() == false)
+  {
+    Serial.println("Module failed to respond. Please check wiring.");
+    while (1); //Freeze!
+  }
+  Serial.println("GPS module found!");
+}
+
+void loop()
+{
+  while (myI2CGPS.available()) //available() returns the number of new bytes available from the GPS module
+  {
+    gps.encode(myI2CGPS.read()); //Feed the GPS parser
+  }
+
+  if (gps.time.isUpdated()) //Check to see if new GPS info is available
+  {
+    displayInfo();
+  }
+}
+
+//Display new GPS info
+void displayInfo()
+{
+  //We have new GPS data to deal with!
+  Serial.println();
+
+  if (gps.time.isValid())
+  {
+    Serial.print(F("Date: "));
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+
+    Serial.print((" Time: "));
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+
+    Serial.println(); //Done printing time
+  }
+  else
+  {
+    Serial.println(F("Time not yet valid"));
+  }
+
+  if (gps.location.isValid())
+  {
+    Serial.print("Location: ");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(", "));
+    Serial.print(gps.location.lng(), 6);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println(F("Location not yet valid"));
+  }
+}
+
