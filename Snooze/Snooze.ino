7a61f65582d8c483e3ebfe85fd88b1a8eccd1ce9
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-01-31 13:46:12 -0600

    working on snooze

--- Snooze/Snooze.ino
+++ Snooze/Snooze.ino
@@ -1,94 +1,49 @@
-/***************************************
- This shows all the wakeups for sleep
- Expect IDD of  around 1.2mA (Teensy 3.x)
- and IDD of around 900uA for (Teensy LC).
- 
- Sleep is the most flexable and any
- interrupt can wake the processor.
- 
- Touch interface does not work in sleep
- mode.
- ****************************************/
+
+
+#include <SparkFun_I2C_GPS_Arduino_Library.h> //Use Library Manager or download here: https://github.com/sparkfun/SparkFun_I2C_GPS_Arduino_Library
+I2CGPS myI2CGPS; //Hook object to the library
+
+#include <TinyGPS++.h> //From: https://github.com/mikalhart/TinyGPSPlus
+TinyGPSPlus gps; //Declare gps object
 #include <Snooze.h>
+#include <NSET.h>
 // Load drivers
 SnoozeDigital digital;
 SnoozeCompare compare;
 SnoozeTimer timer;
 SnoozeAlarm  alarm;
-// configures the lc's 5v data buffer (OUTPUT, LOW) for low power
-Snoozelc5vBuffer  lc5vBuffer;
-/***********************************************************
- Teensy 3.6/LC can't use Timer Driver with either Touch or
- Compare Drivers and Touch can't be used with Compare.
- 
- Teensy 3.x/LC touch interface does not work with sleep.
- 
- Teensy LC does not have a rtc so Alarm driver can't be
- used as of yet.
- 
- Teensy 3.2 can use any Core Drivers together.
- ***********************************************************/
-#if defined(__MK66FX1M0__)
-SnoozeBlock config_teensy36(digital, alarm, compare);
-#elif defined(__MK64FX512__)
-SnoozeBlock config_teensy35(digital, timer, compare);
-#elif defined(__MK20DX256__)
-SnoozeBlock config_teensy32(digital, timer, compare);
-#elif defined(__MK20DX128__)
-SnoozeBlock config_teensy30(digital, timer, compare);
-#elif defined(__MKL26Z64__)
-SnoozeBlock config_teensyLC(digital, timer, lc5vBuffer);
-#endif
+NSET  SD1;
+
+#define ALARM_S  20
+SnoozeBlock config1(digital, timer);
+
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    if (myI2CGPS.begin() == false)
+    {
+      //Serial.println("Module failed to respond. Please check wiring.");
+      while (1); //Freeze!
+    }
+    pinMode(LED_BUILTIN,HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    
     /********************************************************
      Define digital pins for waking the teensy up. This
      combines pinMode and attachInterrupt in one function.
      
      Teensy 3.x
      Digtal pins: all pins
-     
-     Teensy LC
-     Digtal pins: all interrupt able pins
      ********************************************************/
-    digital.pinMode(21, INPUT_PULLUP, RISING);//pin, mode, type
-    digital.pinMode(22, INPUT_PULLUP, RISING);//pin, mode, type
+    digital.pinMode(22, INPUT_PULLUP, FALLING);//pin, mode, type
     
     /********************************************************
      Teensy 3.x only currently.
      
      Set RTC alarm wake up in (hours, minutes, seconds).
      ********************************************************/
-    alarm.setRtcTimer(0, 0, 10);// hour, min, sec
-    
-    /********************************************************
-     Set Low Power Timer wake up in milliseconds.
-     ********************************************************/
-    timer.setTimer(5000);// milliseconds
-    
-    /********************************************************
-    In sleep the Compare module works by setting the
-    internal 6 bit DAC to a volatge threshold and monitors
-    the pin volatge for a voltage crossing. The internal
-    DAC uses a 64 tap resistor ladder network supplied by
-    VOUT33 at 0.0515625v per tap (VOUT33/64). Thus the
-    possible threshold voltages are 0.0515625*(0-64). Only
-    one compare pin can be used at a time.
-
-    parameter "type": LOW & FALLING are the same and have no effect.
-    parameter "type": HIGH & RISING are the same and have no effect.
-
-    Teensy 3.x
-    Compare pins: 11,9,4
-
-    Teensy LC
-    Compare pins: 11
-     ********************************************************/
-    // trigger at threshold values greater than 1.65v
-    //compare.pinMode(11, HIGH, 1.65);//pin, type, threshold(v)
-    // trigger at threshold values less than 1.65v
-    compare.pinMode(11, LOW, 1.65);//pin, type, threshold(v)
+    //timer.setTimer(20000);// milliseconds
 }
 
 void loop() {
@@ -97,60 +52,70 @@ void loop() {
      feed the sleep function its wakeup parameters. Then go
      to deepSleep.
      ********************************************************/
-#if defined(__MK66FX1M0__)
-    who = Snooze.sleep( config_teensy36 );// return module that woke processor
-#elif defined(__MK64FX512__)
-    who = Snooze.sleep( config_teensy35 );// return module that woke processor
-#elif defined(__MK20DX256__)
-    who = Snooze.sleep( config_teensy32 );// return module that woke processor
-#elif defined(__MK20DX128__)
-    who = Snooze.sleep( config_teensy30 );// return module that woke processor
-#elif defined(__MKL26Z64__)
-    who = Snooze.sleep( config_teensyLC );// return module that woke processor
-#endif
-    
-    if (who == 21) { // pin wakeup source is its pin value
-        for (int i = 0; i < 1; i++) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(200);
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(200);
-        }
+    who = Snooze.sleep( config1 );// return module that woke processor
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if(!SD1.SDWrite(String(who))){
+    SD1.SDinit();
+    SD1.SDWrite("failed");  
+  }
+  if (myI2CGPS.available()) //available() returns the number of new bytes available from the GPS module
+  {
+    gps.encode(myI2CGPS.read()); //Feed the GPS parser
+  }
+
+  if (gps.time.isUpdated()) //Check to see if new GPS info is available
+  {
+    displayInfo();
+  }
+}
+
+
+
+
+String displayInfo()
+{
+  //We have new GPS data to deal with!
+  String data;
+  if (gps.time.isValid())
+  {
+    data = "Date: ";
+    data+= String(gps.date.month());
+    data+= "/";
+    data+= String(gps.date.day());
+    data+= "/";
+    data+= String(gps.date.year());
+    data+= " Time: ";
+    if (gps.time.hour() < 10) data+="0";
+    data += String(gps.time.hour());
+    data += ":";
+    if (gps.time.minute() < 10) data+="0";
+    data+=String(gps.time.minute());
+    data+=":";
+    if (gps.time.second() < 10) data+="0";
+    data+=String(gps.time.second());
+    if(!SD1.SDWrite(data)){
+      SD1.SDinit();
+      SD1.SDWrite(data);
     }
-    
-    if (who == 22) { // pin wakeup source is its pin value
-        for (int i = 0; i < 2; i++) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(200);
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(200);
-        }
-    }
-    
-    if (who == 34) { // compare wakeup value
-        for (int i = 0; i < 3; i++) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(200);
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(200);
-        }
-    }
-    
-    if (who == 35) { // rtc wakeup value
-        for (int i = 0; i < 4; i++) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(200);
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(200);
-        }
-    }
-    
-    if (who == 36) { // lptmr wakeup value
-        for (int i = 0; i < 5; i++) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(200);
-            digitalWrite(LED_BUILTIN, LOW);
-            delay(200);
-        }
-    }
+  }
+
+//    Serial.println(); //Done printing time
+//  }
+//  else
+//  {
+//    Serial.println(F("Time not yet valid"));
+//  }
+//
+//  if (gps.location.isValid())
+//  {
+//    Serial.print("Location: ");
+//    Serial.print(gps.location.lat(), 6);
+//    Serial.print(F(", "));
+//    Serial.print(gps.location.lng(), 6);
+//    Serial.println();
+//  }
+//  else
+//  {
+//    Serial.println(F("Location not yet valid"));
+//  }
 }
